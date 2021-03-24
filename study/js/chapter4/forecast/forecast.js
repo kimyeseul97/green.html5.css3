eab9900493199af3c4a99a7f476f3fe2c4924f84
github kimyeseul97 <github yebasang97@gmail.com>
2021-03-24 19:00:43 +0900

    3월24일 수업내용

--- study/js/chapter4/forecast/forecast.js
+++ study/js/chapter4/forecast/forecast.js
@@ -1,85 +1,67 @@
-var date = ["21-03-22","21-03-23","21-03-24","21-03-25"];
+var date = ["21-03-22", "21-03-23", "21-03-24", "21-03-25"];
 var temp = [10.2, 14.3, 17.2, 18.0];
-var rain = [10,50,10,5];
-var dust = [40,60,70,90];
-var day1 = [], day2 = [], day3=[], day4=[];
-var forcast = new Object();
+var rain = [10, 50, 10, 5];
+var dust = [40, 60, 70, 90];
+var day1 = [], day2 = [], day3 = [], day4 = [];
+var days = [day1, day2, day3, day4];
+//var forecast = new Object();
 
-forecast.tempShow = function(){
-    for(var i = 0; i < temp.length; i++){
-
+function Forecast(arr) {
+    this.foreCastArr = arr;
+    switch (arr) {
+        case temp: this.division = "temp";
+            break;
+        case rain: this.division = "rain";
+            break;
+        case dust: this.division = "dust";
+            break;    
     }
+}
+
+Forecast.prototype.show = function(){
+    var str = "";
+    for(var i = 0; i < this.foreCastArr.length; i++){
+        str += this.foreCastArr[i] + this.unit(this.foreCastArr) + " "; 
+    }
+    return str;
 };
 
-forecast.temp_Avg_Show = function(){
+Forecast.prototype.avgShow = function(){
     var num = 0;
-    for(var i = 0; i < temp.length; i++){
-        num += temp[i];
+    for(var i = 0; i < this.foreCastArr.length; i++){
+        num += this.foreCastArr[i];
     }
 
-    var avg = num / temp.length;
-    return avg;
+    var avg = num / this.foreCastArr.length;
+    
+    return avg.toFixed(1) + this.unit(this.foreCastArr); //소수점첫째자리까지
 };
 
-forecast.temp_rain_Show = function(){
-    for(var i = 0; i < rain.length; i++){
-
+Forecast.prototype.unit = function(){
+    switch (this.division) {
+        case "temp": return "℃"; 
+            break;
+        case "rain": return "mm";
+            break;
+        case "dust": return "㎍/㎥";
+            break;    
     }
-};
+}
 
-forecast.temp_Avg_rainShow = function(){
-    var num = 0;
-    for(var i = 0; i < rain.length; i++){
-        num += rain[i];
-    }
+Forecast.prototype.innerHtml = function() {
+    var showId = "show_" + this.division;
+    var showAvgId = "show_" + this.division + "_avg";
 
-    var avg = num / rain.length;
-    return avg;
-
-};
-
-forecast.temp_dust_Show = function(){
-    for(var i = 0; i < dust.length; i++){
-
-    }
-};
-
-forecast.temp_dust_Avg_Show = function(){
-    var num = 0;
-    for(var i = 0; i < dust.length; i++){
-        num += dust[i];
-    }
-
-    var avg = num / dust.length;
-    return avg;
-};
+    document.getElementById(showId).innerHTML = this.show();
+    document.getElementById(showAvgId).innerHTML = this.avgShow();
+}
 
 
+/* 함수호출 */
+var temp = new Forecast(temp);
+var rain = new Forecast(rain);
+var dust = new Forecast(dust);
 
-
-// 내가 만든 코드
-// document.write("<h1>1일 날씨 정보</h1>");
-// document.write("현재날짜:"+date[0],"<br>");
-// document.write("온도:"+temp[0],"<br>");
-// document.write("강수량:"+rain[0],"<br>");
-// document.write("미세먼지:"+dust[0],"<br>");
-
-// document.write("<h1>2일 날씨 정보</h1>");
-// document.write("현재날짜:"+date[1],"<br>");
-// document.write("온도:"+temp[1],"<br>");
-// document.write("강수량:"+rain[1],"<br>");
-// document.write("미세먼지:"+dust[1],"<br>");
-
-// document.write("<h1>3일 날씨 정보</h1>");
-// document.write("현재날짜:"+date[2],"<br>");
-// document.write("온도:"+temp[2],"<br>");
-// document.write("강수량:"+rain[2],"<br>");
-// document.write("미세먼지:"+dust[2],"<br>");
-
-// document.write("<h1>4일 날씨 정보</h1>");
-// document.write("현재날짜:"+date[3],"<br>");
-// document.write("온도:"+temp[3],"<br>");
-// document.write("강수량:"+rain[3],"<br>");
-// document.write("미세먼지:"+dust[3],"<br>");
-
-
+temp.innerHtml();
+rain.innerHtml();
+dust.innerHtml();
