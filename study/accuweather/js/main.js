3e907bca2e4b832f091dad1f875c7f1f03b6658d
kimyeseul97 <yebasang97@gmail.com>
2021-04-13 18:42:15 +0900

    0413

--- study/accuweather/js/main.js
+++ study/accuweather/js/main.js
@@ -1,15 +1,29 @@
 $(function(){
-    $(".nav li").hover(function() {
-        var lnbShow = $(".lnb_container").is("visible");
-        
-        if(lnbShow) { // 하위 메뉴가 보이면
+    var gnbFlag = false;
+    var gnbIndex;
+
+    $(".nav li").on(function() {
+        "mouseenter" : function() {
             $(".lnb_container").removeClass("appear");
             $(".lnb_container").addClass("disappear");
-        } else { //하위 메뉴가 보이지 않으면
+            $(".lnb_container").show();
+        },
+        "mouseleave" : function() {
             $(".lnb_container").removeClass("disappear");
-            $(".lnb_container").addClass("appear");        
+            $(".lnb_container").addClass("appear");
         }
-
-        $(".lnb_contaier").toggle(); //block 혹은 none 처리
     });
-});
+
+//         var lnbShow = $(".lnb_container").is("visible");
+        
+//         if(lnbShow) { // 하위 메뉴가 보이면
+//             $(".lnb_container").removeClass("appear");
+//             $(".lnb_container").addClass("disappear");
+//         } else { //하위 메뉴가 보이지 않으면
+//             $(".lnb_container").removeClass("disappear");
+//             $(".lnb_container").addClass("appear");        
+//         }
+
+//         $(".lnb_contaier").toggle(); //block 혹은 none 처리
+//     });
+// });
